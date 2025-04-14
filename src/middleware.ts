388166ead54410124dd07483dc8bff838b9913ca
cristianQuev <119689796+cristianQuev@users.noreply.github.com>
2025-04-14 10:00:32 -0500

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,6 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ["/", "/(es|en)/:path*"],
+  // matcher: ["/", "/(es|en)/:path*"],
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
