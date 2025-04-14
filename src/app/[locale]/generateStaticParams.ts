f050ac9d32afc2550eef3338043efb1b77bfe8cf
cristianQuev <119689796+cristianQuev@users.noreply.github.com>
2025-04-14 08:37:59 -0500

    remove wrong file

--- src/app/[locale]/generateStaticParams.ts
+++
@@ -1,5 +0,0 @@
-export function generateStaticParams() {
-  const locales = ["en", "es", "fr"];
-  console.log("⚡ Generando rutas para:", locales);
-  return locales.map((locale) => ({ locale }));
-}
