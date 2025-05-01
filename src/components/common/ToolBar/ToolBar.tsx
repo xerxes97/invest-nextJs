50e22e7227bd4b86faafce85a04e7130cbd7ca47
cristianQuev <119689796+cristianQuev@users.noreply.github.com>
2025-05-01 15:04:56 -0500

    padding fixed in mobile screen

--- src/components/common/ToolBar/ToolBar.tsx
+++ src/components/common/ToolBar/ToolBar.tsx
@@ -1,9 +1,15 @@
+"use client";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { ToolBarOpcContainer } from "./components";
 
 export const AppToolBar = () => {
+  const isMobile = useIsMobile();
+  const classScreen = isMobile ? "mx-2" : " mr-2";
   return (
     <div className="h-12 mt-2">
-      <div className="border border-[var(--sidebar-border)] shadow-sm flex items-center justify-end bg-[var(--sidebar)] rounded-lg mr-2 h-full">
+      <div
+        className={`border border-[var(--sidebar-border)] shadow-sm flex items-center justify-end bg-[var(--sidebar)] rounded-lg h-full ${classScreen}`}
+      >
         <ToolBarOpcContainer />
       </div>
     </div>
